df4267a091d49aa34e966107b7704d0983de4d0a
daothuc <dthuc658@gmail.com>
2025-05-18 18:02:20 +0700

    push end

---
+++ MOHO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MOHO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MOHO/Services/IVnPayService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using MOHO.Models;
+
+namespace MOHO.Services;
+public interface IVnPayService
+{
+    string CreatePaymentUrl(PaymentInformationModel model, HttpContext context);
+    PaymentResponseModel PaymentExecute(IQueryCollection collections);
+}
